dc31a11f4f83feab60fd5ccc928cf3d67ac9922a
petarkrivic <petarkrivic@10.19.4.102>
2017-04-05 19:08:17 +0200

    Task model specification first version. Added SymbioteLibraries to enablerLogic project (observation model needed).

---
+++ src/main/java/eu/h2020/symbiote/model/Task.java
@@ -0,0 +1,56 @@
+package eu.h2020.symbiote.model;
+
+import org.springframework.data.annotation.Id;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import eu.h2020.symbiote.core.model.Observation;
+
+/**
+ * Task abstraction model
+ *
+ * Created by Petar Krivic.
+ */
+public class Task {
+	
+	@Id
+	@JsonProperty("taskId")
+	private String taskId;
+	
+	@JsonProperty("timestamp")
+	private String timestamp;		//not sure if String or something else..
+	
+	@JsonProperty("resourceId")
+	private String resourceId;
+	
+	@JsonProperty("data")			//as proposed in confluence, could be also observation..
+	private Observation observation;
+	
+	public Task(@JsonProperty("taskId")String taskId,
+				@JsonProperty("timestamp")String timestamp,
+				@JsonProperty("resourceId")String resourceId, 
+				@JsonProperty("data")Observation data){
+		this.taskId = taskId;
+		this.timestamp = timestamp;
+		this.resourceId = resourceId;
+		this.observation = data;
+	}
+
+	public String getTaskId() {
+		return taskId;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public String getResourceId() {
+		return resourceId;
+	}
+
+	public Observation getObservation() {
+		return observation;
+	}
+
+	
+}
