5cd8c7a5c7ce75dcba8f7f4d9106a9f7c06d0001
mkusek <mario.kusek@fer.hr>
2017-08-24 16:01:45 +0200

    Added sending async message to another EnablerLogic

--- src/main/java/eu/h2020/symbiote/EnablerLogic.java
+++ src/main/java/eu/h2020/symbiote/EnablerLogic.java
@@ -11,35 +11,45 @@ import eu.h2020.symbiote.enabler.messaging.model.ResourceManagerAcquisitionStart
 import eu.h2020.symbiote.enabler.messaging.model.ResourceManagerTaskInfoRequest;
 import eu.h2020.symbiote.messaging.RabbitManager;
 import eu.h2020.symbiote.messaging.properties.EnablerLogicProperties;
-import eu.h2020.symbiote.messaging.properties.ResourceManagerExchangeProperties;
 
 @Service
 public class EnablerLogic {
-	private static final Logger log = LoggerFactory.getLogger(EnablerLogic.class);
-	
-	private RabbitManager rabbitManager;
+    private static final Logger log = LoggerFactory.getLogger(EnablerLogic.class);
+    
+    private RabbitManager rabbitManager;
 
-	private EnablerLogicProperties props;
-	
-	public EnablerLogic(RabbitManager rabbitManager, EnablerLogicProperties props) {
-		this.rabbitManager = rabbitManager;
-		this.props = props;
-	}
+    private EnablerLogicProperties props;
+    
+    public EnablerLogic(RabbitManager rabbitManager, EnablerLogicProperties props) {
+        this.rabbitManager = rabbitManager;
+        this.props = props;
+    }
 
-	public ResourceManagerAcquisitionStartResponse queryResourceManager(ResourceManagerTaskInfoRequest...requests) {
-		for(ResourceManagerTaskInfoRequest request: requests) {
-			log.info("sending message to ResourceManager: {}", request);
-		}
-		
-		ResourceManagerAcquisitionStartRequest request = new ResourceManagerAcquisitionStartRequest();
-		request.setResources(Arrays.asList(requests));
+    public ResourceManagerAcquisitionStartResponse queryResourceManager(ResourceManagerTaskInfoRequest...requests) {
+        for(ResourceManagerTaskInfoRequest request: requests) {
+            log.info("sending message to ResourceManager: {}", request);
+        }
+        
+        ResourceManagerAcquisitionStartRequest request = new ResourceManagerAcquisitionStartRequest();
+        request.setResources(Arrays.asList(requests));
 
-		ResourceManagerAcquisitionStartResponse response = (ResourceManagerAcquisitionStartResponse) 
-				rabbitManager.sendRpcMessage(props.getExchange().getResourceManager().getName(), 
-						props.getKey().getResourceManager().getStartDataAcquisition(), 
-						request);
-		
-		log.info("Received resourceIds from ResourceManager");
-		return response;
-	}
+        ResourceManagerAcquisitionStartResponse response = (ResourceManagerAcquisitionStartResponse) 
+                rabbitManager.sendRpcMessage(props.getExchange().getResourceManager().getName(), 
+                        props.getKey().getResourceManager().getStartDataAcquisition(), 
+                        request);
+        
+        log.info("Received resourceIds from ResourceManager");
+        return response;
+    }
+    
+    public void sendAsyncMessageToEnablerLogic(String enablerName, Object msg) {
+    		rabbitManager.sendMessage(props.getEnablerLogicExchange().getName(), 
+    		        generateAsyncEnablerLogicRoutingKey(), 
+    		        msg);
+    }
+
+    private String generateAsyncEnablerLogicRoutingKey() {
+        return props.getKey().getEnablerLogic().getAsyncMessageToEnablerLogic()+"." + 
+                props.getEnablerName();
+    }
 }
--- src/test/java/eu/h2020/symbiote/EnablerLogicTest.java
+++ src/test/java/eu/h2020/symbiote/EnablerLogicTest.java
@@ -10,10 +10,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.amqp.core.Message;
 
 import eu.h2020.symbiote.enabler.messaging.model.ResourceManagerAcquisitionStartRequest;
 import eu.h2020.symbiote.enabler.messaging.model.ResourceManagerAcquisitionStartResponse;
@@ -23,36 +21,52 @@ import eu.h2020.symbiote.messaging.properties.EnablerLogicProperties;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EnablerLogicTest {
-	private EnablerLogic enablerLogic;
-	
-	@Mock
-	RabbitManager rabbitManager;
+    private EnablerLogic enablerLogic;
+    
+    @Mock
+    RabbitManager rabbitManager;
 
-	@Before
-	public void setup() {
-		enablerLogic = new EnablerLogic(rabbitManager, new EnablerLogicProperties());
-	}
-	
-	@Test
-	public void queryResourceManagerWithOneRequest_shouldReturnResponse() throws Exception {
-		// given
-		ArgumentCaptor<ResourceManagerAcquisitionStartRequest> captor = ArgumentCaptor.forClass(ResourceManagerAcquisitionStartRequest.class);
-		ResourceManagerTaskInfoRequest request = new ResourceManagerTaskInfoRequest();
-		ResourceManagerAcquisitionStartResponse response = new ResourceManagerAcquisitionStartResponse();
-		when(rabbitManager.sendRpcMessage(
-				eq("symbIoTe.resourceManager"), 
-				eq("symbIoTe.resourceManager.startDataAcquisition"), 
-				any(ResourceManagerAcquisitionStartRequest.class))).thenReturn(response);
-		
-		// when
-		enablerLogic.queryResourceManager(request);
-		
-		// then
-		verify(rabbitManager).sendRpcMessage(eq("symbIoTe.resourceManager"), 
-				eq("symbIoTe.resourceManager.startDataAcquisition"), 
-				captor.capture());
-		ResourceManagerAcquisitionStartRequest requests = captor.getValue();
-		assertThat(requests.getResources()).hasSize(1);
-		assertThat(requests.getResources()).contains(request);
-	}
+    @Before
+    public void setup() {
+        enablerLogic = new EnablerLogic(rabbitManager, new EnablerLogicProperties());
+    }
+    
+    @Test
+    public void queryResourceManagerWithOneRequest_shouldReturnResponse() throws Exception {
+        // given
+        ArgumentCaptor<ResourceManagerAcquisitionStartRequest> captor = ArgumentCaptor.forClass(ResourceManagerAcquisitionStartRequest.class);
+        ResourceManagerTaskInfoRequest request = new ResourceManagerTaskInfoRequest();
+        ResourceManagerAcquisitionStartResponse response = new ResourceManagerAcquisitionStartResponse();
+        when(rabbitManager.sendRpcMessage(
+                eq("symbIoTe.resourceManager"), 
+                eq("symbIoTe.resourceManager.startDataAcquisition"), 
+                any(ResourceManagerAcquisitionStartRequest.class))).thenReturn(response);
+        
+        // when
+        enablerLogic.queryResourceManager(request);
+        
+        // then
+        verify(rabbitManager).sendRpcMessage(eq("symbIoTe.resourceManager"), 
+                eq("symbIoTe.resourceManager.startDataAcquisition"), 
+                captor.capture());
+        ResourceManagerAcquisitionStartRequest requests = captor.getValue();
+        assertThat(requests.getResources()).hasSize(1);
+        assertThat(requests.getResources()).contains(request);
+    }
+    
+    @Test
+    public void sendingSyncMessageToEnablerLogic_shouldCallRabbitManager() throws Exception {
+        // given
+        //ArgumentCaptor<Object> captor = ArgumentCaptor.forClass(Object.class);
+        String message = "test message";
+        String enablerName = "enabler name";
+        
+        // when
+        enablerLogic.sendAsyncMessageToEnablerLogic(enablerName, message);
+        
+        // then
+        verify(rabbitManager).sendMessage(eq("symbIoTe.enablerLogic"), 
+                eq("symbIoTe.enablerLogic.asyncMessageToEnablerLogic.DefaultEnablerName"), 
+                eq((Object)message));
+    }
 }
